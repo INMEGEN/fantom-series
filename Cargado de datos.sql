852680c9669e5921f1731ded5c4279e1f3953c54
alexjair123 <alexjair1@hotmail.com>
2016-06-24 15:09:38 -0500

    Comandos para cargar los datos de los archivos en sql

---
+++ Cargado de datos.sql
@@ -0,0 +1,17 @@
+.separator "\t";
+.import edge.miRNA_targets.tbl.txt miRNA_targets 
+.import edge.ChIP_chip.tbl.txt ChIP_chip 
+.import edge.GoldStd_TF.tbl.txt GoldStd_TF 
+.import edge.L2_to_L3_april08.tbl.txt L2_to_L3_april08
+.import edge.L3_promoter_Entrez_08May16_EvN.tbl.txt L3_promoter_Entrez_08May16_EvN
+.import edge.miRNA_pre2mature.tbl.txt miRNA_pre2mature
+.import edge.pre-miRNA_perturbation.tbl.txt pre_miRNA_perturbation
+.import edge.public_ChIP_chip.tbl.txt PubmedID
+.import edge.siRNA_perturbation.tbl.txt siRNA_perturbation
+.import edge2hop.Entrez_TFmatrix_L2_may08.tbl.txt TFmatrix_L2_may08
+.import feature.CAGE_L1_promoter.tbl.txt CAGE_L1_promoter
+.import feature.CAGE_L2_promoter.tbl.txt CAGE_L2_promoter_april2008
+.import feature.CAGE_L3_promoter.tbl.txt CAGE_L3_promoter_april2008
+.import feature.entrez_gene.tbl.txt Entrez_gene
+.import feature.miRBase_mature.tbl.txt miRBase_mature
+.import feature.miRBase_pre.tbl.txt miRBase_pre
